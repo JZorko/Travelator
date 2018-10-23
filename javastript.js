a875ecaa82368c213335a794f58169b005ae14e0
zigapusnik12 <44377970+zigapusnik12@users.noreply.github.com>
2018-10-23 20:02:44 +0200

    Autocomplete

--- javastript.js
+++ javastript.js
@@ -1,26 +1,37 @@
 function initMap() {
-	var marker = new google.maps.Marker();
 	var options = {
 		zoom: 12,
 		disableDefaultUI: true,
 		zoomControl: true,
 	}
 	var map = new google.maps.Map(document.getElementById('map'), options);
-    var acOptions = {
-    	types: ['establishment']
-    };
-    var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
-    autocomplete.bindTo('bounds', map);
-    var infoWindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-    	map: map
-    });
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-    	infoWindow.close();
-    	var place = autocomplete.getPlace();
-    	marker.setPosition(place.geometry.location);
-    	map.setCenter(place.geometry.location);
-    })
+  var acOptions = {
+  	types: ['establishment']
+  };
+  var origin = new google.maps.places.Autocomplete(document.getElementById('origin'));
+	var destination = new google.maps.places.Autocomplete(document.getElementById('destination'));
+  origin.bindTo('bounds', map);
+	destination.bindTo('bounds', map);
+  var infoWindow = new google.maps.InfoWindow();
+  var marker_origin = new google.maps.Marker({
+  	map: map
+  });
+	var marker_destination = new google.maps.Marker({
+  	map: map
+  });
+  google.maps.event.addListener(origin, 'place_changed', function () {
+  	infoWindow.close();
+  	var place = origin.getPlace();
+  	marker_origin.setPosition(place.geometry.location);
+  	map.setCenter(place.geometry.location);
+  });
+	google.maps.event.addListener(destination, 'place_changed', function () {
+  	infoWindow.close();
+  	var place = destination.getPlace();
+  	marker_destination.setPosition(place.geometry.location);
+  	map.setCenter(place.geometry.location);
+	});
+
 	/*google.maps.event.addListener(marker, 'click', function(event){
 			infoWindow.setContent('kliknil si me ');
 			infoWindow.open(map, marker);
@@ -45,27 +56,27 @@ function initMap() {
 			}
 		})
 	}*/
-    if (navigator.geolocation) {
-      	navigator.geolocation.getCurrentPosition(
-      		function(position) {
-	            var pos = {
-	              	lat: position.coords.latitude,
-	              	lng: position.coords.longitude
-	            };
-	            map.setCenter(pos);
-	          	},
-	        function() {
-	            handleLocationError(true, infoWindow, map.getCenter());
-	        }
-    	);
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+  if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(
+    		function(position) {
+            var pos = {
+              	lat: position.coords.latitude,
+              	lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+          	},
+        function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        }
+  	);
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-    // This event listener calls addMarker() when the map is clicked.
+  // This event listener calls addMarker() when the map is clicked.
 
-    var modal_reg = document.getElementById('registerForm');
+  var modal_reg = document.getElementById('registerForm');
 	var modal_log = document.getElementById('loginForm');
 	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
@@ -84,6 +95,9 @@ function initMap() {
 	document.getElementById('btn_log').onclick= function () {
 		document.getElementById('loginForm').style.display="block";
 		modal_reg.style.display = "none";
+	}
+	document.getElementById('btn_cal').onclick= function () {
+		
 	}
 
 }
