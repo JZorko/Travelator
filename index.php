04973f873a99734cf7c0fe61ef05167ba58219e4
Jan Zorko <jzorko2000@gmail.com>
2018-10-24 20:42:10 +0200

    Car section

--- index.php
+++ index.php
@@ -115,7 +115,6 @@
         <ul>
           <li id="btn_log">Login</li>
           <li id="btn_reg">Register</li>
-					<li id="btn_vnos">Add a Car</li>
         </ul>
       </div>
 
@@ -125,6 +124,13 @@
           <li id="btn_logout">Logout</li>
         </ul>
       </div>
+
+			<div class="login">
+				<h4 class="header">Cars</h4>
+				<ul>
+					<li id="btn_vnos">Add a Car</li>
+				</ul>
+			</div>
     </aside>
 		<div id="mapContainer">
 			<div id="map"></div>
